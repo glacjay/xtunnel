2b25fe65c2d11633624735afb2786930bb186899
Jay True <glacjay@gmail.com>
2010-01-19 00:10:21 +0800

    change from msnp to msnlib but still not working yet

--- xtunnel/im/msn_proto.py
+++ xtunnel/im/msn_proto.py
@@ -1,67 +1,93 @@
 import base64
-import msnp
+import msncb
+import msnlib
+import sys
 import threading
 import time
 
 
-class MessageHandler(msnp.ChatCallbacks):
-
-    def __init__(self, client, chat):
-        self.client = client
-        self.peer = client.peer
-        self.chat = chat
-
-    def message_received(self, passport, display, text, charset):
-        if passport == self.peer:
-            self.client.writer.write(base64.b64decode(text))
-
-    def friend_left(self, passport):
-        self.client.chat = None
-
-
-class MSNClient(threading.Thread, msnp.SessionCallbacks):
+class MSNClient(threading.Thread):
 
     def __init__(self, config, writer=None):
         threading.Thread.__init__(self)
-        self.msn = msnp.Session(self)
-        self.account = config['account']
-        self.password = config['password']
+        self.m = msnlib.msnd()
+        self.m.cb = msncb.cb()
+        self.m.email = config['account']
+        self.m.pwd = config['password']
         self.peer = config['peer']
+        self.peer_online = False
+
+        self.setup_callbacks()
+
+    def setup_callbacks(self):
+        for p in dir(self):
+            if p.startswith('cb_'):
+                setattr(self.m.cb, p[3:], getattr(self, p))
 
     def connect(self):
         try:
-            self.msn.login(self.account, self.password)
-            self.msn.change_state(msnp.States.ONLINE)
+            self.m.login()
+            self.m.sync()
+            self.m.change_status('online')
             print 'Connecting succeeded.'
             return True
         except:
             print 'Failed to connect or authenticate.'
             return False
 
-    def friend_online(self, state, passport, display):
-        print '%s %s' % (passport, state)
-        if state == msnp.States.ONLINE and passport == self.peer:
-            self.msn.start_chat(self.peer)
-
-    def friend_offline(self, passport):
-        if passport == self.peer:
-            self.chat = None
-
-    def chat_started(self, chat):
-        print 'chat started'
-        self.chat = chat
-        callbacks = MessageHandler(self, chat)
-        chat.callbacks = callbacks
-
     def write(self, message):
-        print 'write',
-        if hasattr(self, 'chat'):
-            self.chat.send_message(base64.b64encode(message))
+        if self.peer_online:
+            self.m.sendmsg(self.peer, base64.b64encode(message))
 
     def fileno(self):
-        return self.msn.conn.socket._sock
+        return self.m.fileno()
 
     def run(self):
         while True:
-            self.msn.process(chats=True)
-            time.sleep(200)
+            self.m.read(self.m)
+            self.m.sync()
+
+    #
+    # callbacks
+    #
+    def cb_fln(self, md, type, tid, params):
+        email = tid
+        print >> sys.stderr, '*** fln: %s: offline' % email
+
+        if email == self.peer:
+            self.peer_online = False
+
+        msncb.cb_fln(md, type, tid, params)
+
+    def cb_iln(self, md, type, tid, params):
+        t = params.split(' ')
+        status = msnlib.reverse_status[t[0]]
+        email = t[1]
+        print >> sys.stderr, '*** iln: %s: %s' % (email, status)
+
+        msncb.cb_iln(md, type, tid, params)
+
+    def cb_msg(self, md, type, tid, params, sbd):
+        t = tid.split(' ')
+        email = t[0]
+        print >> sys.stderr, '*** msg: %s: %s' % (email, params)
+        msncb.cb_msg(md, type, tid, params, sbd)
+
+    def cb_nln(self, md, type, tid, params):
+        status = msnlib.reverse_status[tid]
+        t = params.split(' ')
+        email = t[0]
+        print >> sys.stderr, '*** nln: %s: %s' % (email, status)
+
+        if email == self.peer:
+            self.peer_online = True
+
+        msncb.cb_nln(md, type, tid, params)
+
+    def cb_rng(self, md, type, tid, params):
+        t = params.split(' ')
+        email = t[3]
+        print >> sys.stderr, '*** rng: %s' % email
+
+        if email == self.peer:
+            msncb.cb_rng(md, type, tid, params)
